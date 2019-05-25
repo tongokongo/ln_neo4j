c704c09f11f0fe0709b2808644990bcbcb979802
root <radmanmen@gmail.com>
2019-05-25 17:58:26 +0000

    edges and nodes methods reworked

--- parse_response.py
+++ parse_response.py
@@ -12,80 +12,50 @@ class response_parser():
         nodes_list = []
         for node in self.nodes:
             #print(node)
-            if node.last_update:
-                last_update = node.last_update
-            else:
-                last_update = None
-
-            if node.alias:
-                alias = node.alias
-            else:
-                alias = None
-
-            if node.color:
-                color = node.color
-            else:
-                color = None
-
-            if node.addresses:
-                if "network" in node.addresses:
-                    network = node.addresses["network"]
-                else:
-                    network = None
-
-                if "addr" in node.addresses:
-                    addr = node.addresses["addr"]
-                else:
-                    addr = None
-            else:
-                addr = None
-                network = None
-
             d = {
                 "pubKey": node.pub_key,
-                "lastUdate" : last_update,
-                "alias" : alias,
-                "color" : color,
-                "network" : network,
-                "addr" : addr
-            }
+                "lastUdate" : node.last_update if node.last_update else None,
+                "alias" : node.alias if node.alias else None,
+                "color" : node.color if node.color else None,
+                "addresses" : node.addresses if node.addresses else None
+                #addresses keys "network, "addr"
+                }
 
             nodes_list.append(d)
-
-
         print(len(nodes_list))
         return nodes_list
 
+    '''channel_id: 635198862672003073
+    chan_point: "f1bcc3df0ddf3a49f1e58c6b17935e4d8f1fe806055e5870a19719889216c5c0:1"
+    last_update: 1558788122
+    node1_pub: "02a04446caa81636d60d63b066f2814cbd3a6b5c258e3172cbdded7a16e2cfff4c"
+    node2_pub: "036a33e92cda2679ac91ac0467acb1ba33396c0df1119521653e0f269c275dd6bf"
+    capacity: 4000000
+    node1_policy {
+      time_lock_delta: 29
+      min_htlc: 1000
+      fee_rate_milli_msat: 1
+      max_htlc_msat: 3960000000
+    }
+    node2_policy {
+      time_lock_delta: 40
+      min_htlc: 1000
+      fee_base_msat: 1000
+      fee_rate_milli_msat: 1
+      max_htlc_msat: 3960000000
+    }'''
     def parseEdges(self):
         edges_list = []
         for edge in self.edges:
             #print(edge)
-            if edge.channel_id:
-                channel_id = edge.channel_id
-            else:
-                channel_id = None
-
-            if edge.chan_point:
-                chan_point = edge.chan_point
-            else:
-                chan_point = None
-
-            if edge.last_update:
-                last_update = edge.last_update
-            else:
-                last_update = None
-
-            if edge.node1_pub:
-                node1_pub = edge.node1_pub
-            else:
-                node1_pub = None
-
-            if edge.node2_pub:
-                node2_pub = edge.node2_pub
-            else:
-                node2_pub = None
-
-            if edge.capacity:
-                capacity = edge.capacity
-            else:
-                capacity = None
+            d = {
+                "chanID": edge.channel_id,
+                "chanPoint": edge.chan_point if edge.chan_point else None,
+                "lastUdate": edge.last_update if edge.last_update else None,
+                "node1Pub": edge.node1_pub if edge.node1_pub else None,
+                "node2Pub": edge.node2_pub if edge.node2_pub else None,
+                "capacity": edge.capacity if edge.capacity else None,
+                "node1Policy": edge.node1_policy if edge.node1_policy else None,
+                "node2Policy": edge.node2_policy if edge.node2_policy else None
+                # nodeXPolicy keys "time_lock_delta", "min_htlc", "fee_rate_milli_msat", "max_htlc_msat", optional "disabled" bool
+            }
