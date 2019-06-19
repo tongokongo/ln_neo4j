ed4a73761cba9278febc19e964bf825d2e1089e4
tongokongo <radmanmen@gmail.com>
2019-06-19 02:46:14 +0000

    Revert "Unwind neo4j statement added"
    This reverts commit 15ab2c20b32f7bfc2bd96245e244b50c6b599405.

--- neo/neo4jobj.py
+++ neo/neo4jobj.py
@@ -15,16 +15,7 @@ class Neo4J:
                             SET n.alias=$dict.alias, n.lastUpdate=$dict.lastUpdate, n.color=$dict.color, 
                             n.network=$dict.network, n.addr=$dict.addr ''',
                        dict = node)
-                #print("Node saved")
 
-    def saveAllNodes(self, nodes):
-        with self._driver.session() as session:
-            with session.begin_transaction() as tx:
-                tx.run('''  UNWIND $dict as d
-                                MERGE (n:LnNode {pubkey: $d.pubKey}) 
-                                SET n.alias=$d.alias, n.lastUpdate=$d.lastUpdate, n.color=$dict.color, 
-                                n.network=$d.network, n.addr=$d.addr ''',
-                       dict = nodes)
                 #print("Node saved")
 
     def saveSingleChannel(self, edge):
@@ -43,24 +34,7 @@ class Neo4J:
                              r2.timeLockDelta=$dict.timeLockDelta2, r2.minHtlc=$dict.minHtlc2,
                              r2.feeBaseMsat=$dict.feeBaseMsat2, r2.feeRateMilliMsat=$dict.feeRateMilliMsat2 ''',
                        dict = edge)
-                #print("Edge saved")
 
-    def saveAllChannels(self, edges):
-        with self._driver.session() as session:
-            with session.begin_transaction() as tx:
-                tx.run('''  UNWIND $dict as d
-                                MERGE (n1:LnNode {pubkey: $d.node1Pub}) 
-                                MERGE (n2:LnNode {pubkey: $d.node2Pub})
-                                MERGE (e:LnChan {point: $d.chanPoint})
-                                MERGE (n1)-[r1:LnNode1Policy]->(e)
-                                MERGE (e)-[r2:LnNode2Policy]->(n2)
-                                SET e.id=$d.chanID, e.capacity=$d.capacity, e.lastUpdate=$d.lastUpdate,\
-                                r1.timeLockDelta=$d.timeLockDelta1, r1.minHtlc=$d.minHtlc1,
-                                r1.feeBaseMsat=$d.feeBaseMsat1, r1.feeRateMilliMsat=$d.feeRateMilliMsat1,
-                                r1.maxHtlcMsat=$d.maxHtlcMsat1, r2.maxHtlcMsat=$d.maxHtlcMsat2,
-                                r2.timeLockDelta=$d.timeLockDelta2, r2.minHtlc=$d.minHtlc2,
-                                r2.feeBaseMsat=$d.feeBaseMsat2, r2.feeRateMilliMsat=$d.feeRateMilliMsat2 ''',
-                       dict = edges)
                 #print("Edge saved")
 
     def deleteAll(self):
